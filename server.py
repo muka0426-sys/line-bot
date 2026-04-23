23986a2815ab33e03e6bdd29ca18ae93d206dfac
muka0426-sys <muka0426@gmail.com>
2026-04-23 13:36:02 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 from flask import Flask, request
 import requests
-import os
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN ="pZSIi1xFujZuwgLBdoiM4HB+qAwCBfFlNA2t61Fz9itykvNdtxgPWzqcquXr6+CBZ96hUjE9tTkmReO4oF+SG1CT+T99O7OMQSM2psuMtkc6lzU9aeKi0veKWWVqj2ZK/2bEm/0kaNmsjoKi6tX/UgdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_ACCESS_TOKEN = "pZSIi1xFujZuwgLBdoiM4HB+qAwCBfFlNA2t61Fz9itykvNdtxgPWzqcquXr6+CBZ96hUjE9tTkmReO4oF+SG1CT+T99O7OMQSM2psuMtkc6lzU9aeKi0veKWWVqj2ZK/2bEm/0kaNmsjoKi6tX/UgdB04t89/1O/w1cDnyilFU="
 
-# LINE webhook
+
 @app.route("/callback", methods=["POST"])
 def callback():
     data = request.json
@@ -16,16 +15,16 @@ def callback():
             reply_token = event["replyToken"]
             user_msg = event["message"]["text"]
 
-            reply_message(reply_token, f"你說：{user_msg}")
+            reply_message(reply_token, f"你說: {user_msg}")
 
     return "OK"
 
 
 def reply_message(reply_token, text):
-headers = {
-    "Content-Type": "application/json",
-   "Authorization": "Bearer " + LINE_CHANNEL_ACCESS_TOKEN
-}
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + LINE_CHANNEL_ACCESS_TOKEN
+    }
 
     body = {
         "replyToken": reply_token,
@@ -37,7 +36,11 @@ headers = {
         ]
     }
 
-    requests.post("https://api.line.me/v2/bot/message/reply", headers=headers, json=body)
+    requests.post(
+        "https://api.line.me/v2/bot/message/reply",
+        headers=headers,
+        json=body
+    )
 
 
 @app.route("/")
